a8c515a1bce2dd8bf6494789f3459af010ff4047
Vanderlei K Silva <vanderleis47@gmail.com>
2021-07-01 20:58:29 -0300

    Add project files.

---
+++ CalculadoraSimples/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿
+namespace CalculadoraSimples
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tela = new System.Windows.Forms.Label();
+            this.soma = new System.Windows.Forms.Button();
+            this.dividi = new System.Windows.Forms.Button();
+            this.multiplica = new System.Windows.Forms.Button();
+            this.subtrai = new System.Windows.Forms.Button();
+            this.n1 = new System.Windows.Forms.Button();
+            this.n3 = new System.Windows.Forms.Button();
+            this.n4 = new System.Windows.Forms.Button();
+            this.n5 = new System.Windows.Forms.Button();
+            this.n2 = new System.Windows.Forms.Button();
+            this.n7 = new System.Windows.Forms.Button();
+            this.n0 = new System.Windows.Forms.Button();
+            this.n9 = new System.Windows.Forms.Button();
+            this.n8 = new System.Windows.Forms.Button();
+            this.n6 = new System.Windows.Forms.Button();
+            this.igual = new System.Windows.Forms.Button();
+            this.limpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tela
+            // 
+            this.tela.BackColor = System.Drawing.Color.White;
+            this.tela.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.tela.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.tela.Location = new System.Drawing.Point(44, 26);
+            this.tela.Name = "tela";
+            this.tela.Size = new System.Drawing.Size(301, 50);
+            this.tela.TabIndex = 0;
+            // 
+            // soma
+            // 
+            this.soma.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.soma.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.soma.Location = new System.Drawing.Point(44, 114);
+            this.soma.Name = "soma";
+            this.soma.Size = new System.Drawing.Size(50, 23);
+            this.soma.TabIndex = 1;
+            this.soma.Text = "+";
+            this.soma.UseVisualStyleBackColor = false;
+            this.soma.Click += new System.EventHandler(this.soma_Click);
+            // 
+            // dividi
+            // 
+            this.dividi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.dividi.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.dividi.Location = new System.Drawing.Point(293, 114);
+            this.dividi.Name = "dividi";
+            this.dividi.Size = new System.Drawing.Size(50, 23);
+            this.dividi.TabIndex = 2;
+            this.dividi.Text = "/";
+            this.dividi.UseVisualStyleBackColor = false;
+            this.dividi.Click += new System.EventHandler(this.dividi_Click);
+            // 
+            // multiplica
+            // 
+            this.multiplica.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.multiplica.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.multiplica.Location = new System.Drawing.Point(210, 114);
+            this.multiplica.Name = "multiplica";
+            this.multiplica.Size = new System.Drawing.Size(50, 23);
+            this.multiplica.TabIndex = 3;
+            this.multiplica.Text = "x";
+            this.multiplica.UseVisualStyleBackColor = false;
+            this.multiplica.Click += new System.EventHandler(this.multiplica_Click);
+            // 
+            // subtrai
+            // 
+            this.subtrai.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.subtrai.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.subtrai.Location = new System.Drawing.Point(127, 114);
+            this.subtrai.Name = "subtrai";
+            this.subtrai.Size = new System.Drawing.Size(50, 23);
+            this.subtrai.TabIndex = 4;
+            this.subtrai.Text = "-";
+            this.subtrai.UseVisualStyleBackColor = false;
+            this.subtrai.Click += new System.EventHandler(this.subtrai_Click);
+            // 
+            // n1
+            // 
+            this.n1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.n1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.n1.Location = new System.Drawing.Point(44, 186);
+            this.n1.Name = "n1";
+            this.n1.Size = new System.Drawing.Size(40, 23);
+            this.n1.TabIndex = 5;
+            this.n1.Text = "1";
+            this.n1.UseVisualStyleBackColor = false;
+            this.n1.Click += new System.EventHandler(this.n1_Click);
+            // 
+            // n3
+            // 
+            this.n3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.n3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.n3.Location = new System.Drawing.Point(173, 186);
+            this.n3.Name = "n3";
+            this.n3.Size = new System.Drawing.Size(40, 23);
+            this.n3.TabIndex = 6;
+            this.n3.Text = "3";
+            this.n3.UseVisualStyleBackColor = false;
+            this.n3.Click += new System.EventHandler(this.n3_Click);
+            // 
+            // n4
+            // 
+            this.n4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.n4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.n4.Location = new System.Drawing.Point(237, 186);
+            this.n4.Name = "n4";
+            this.n4.Size = new System.Drawing.Size(40, 23);
+            this.n4.TabIndex = 7;
+            this.n4.Text = "4";
+            this.n4.UseVisualStyleBackColor = false;
+            this.n4.Click += new System.EventHandler(this.n4_Click);
+            // 
+            // n5
+            // 
+            this.n5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.n5.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.n5.Location = new System.Drawing.Point(303, 186);
+            this.n5.Name = "n5";
+            this.n5.Size = new System.Drawing.Size(40, 23);
+            this.n5.TabIndex = 8;
+            this.n5.Text = "5";
+            this.n5.UseVisualStyleBackColor = false;
+            this.n5.Click += new System.EventHandler(this.n5_Click);
+            // 
+            // n2
+            // 
+            this.n2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.n2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.n2.Location = new System.Drawing.Point(110, 186);
+            this.n2.Name = "n2";
+            this.n2.Size = new System.Drawing.Size(40, 23);
+            this.n2.TabIndex = 9;
+            this.n2.Text = "2";
+            this.n2.UseVisualStyleBackColor = false;
+            this.n2.Click += new System.EventHandler(this.n2_Click);
+            // 
+            // n7
+            // 
+            this.n7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.n7.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.n7.Location = new System.Drawing.Point(110, 235);
+            this.n7.Name = "n7";
+            this.n7.Size = new System.Drawing.Size(40, 23);
+            this.n7.TabIndex = 14;
+            this.n7.Text = "7";
+            this.n7.UseVisualStyleBackColor = false;
+            this.n7.Click += new System.EventHandler(this.n7_Click);
+            // 
+            // n0
+            // 
+            this.n0.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.n0.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.n0.Location = new System.Drawing.Point(303, 235);
+            this.n0.Name = "n0";
+            this.n0.Size = new System.Drawing.Size(40, 23);
+            this.n0.TabIndex = 13;
+            this.n0.Text = "0";
+            this.n0.UseVisualStyleBackColor = false;
+            this.n0.Click += new System.EventHandler(this.n0_Click);
+            // 
+            // n9
+            // 
+            this.n9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.n9.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.n9.Location = new System.Drawing.Point(237, 235);
+            this.n9.Name = "n9";
+            this.n9.Size = new System.Drawing.Size(40, 23);
+            this.n9.TabIndex = 12;
+            this.n9.Text = "9";
+            this.n9.UseVisualStyleBackColor = false;
+            this.n9.Click += new System.EventHandler(this.n9_Click);
+            // 
+            // n8
+            // 
+            this.n8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.n8.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.n8.Location = new System.Drawing.Point(173, 235);
+            this.n8.Name = "n8";
+            this.n8.Size = new System.Drawing.Size(40, 23);
+            this.n8.TabIndex = 11;
+            this.n8.Text = "8";
+            this.n8.UseVisualStyleBackColor = false;
+            this.n8.Click += new System.EventHandler(this.n8_Click);
+            // 
+            // n6
+            // 
+            this.n6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.n6.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.n6.Location = new System.Drawing.Point(44, 235);
+            this.n6.Name = "n6";
+            this.n6.Size = new System.Drawing.Size(40, 23);
+            this.n6.TabIndex = 10;
+            this.n6.Text = "6";
+            this.n6.UseVisualStyleBackColor = false;
+            this.n6.Click += new System.EventHandler(this.n6_Click);
+            // 
+            // igual
+            // 
+            this.igual.BackColor = System.Drawing.Color.Yellow;
+            this.igual.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.igual.Location = new System.Drawing.Point(286, 299);
+            this.igual.Name = "igual";
+            this.igual.Size = new System.Drawing.Size(57, 23);
+            this.igual.TabIndex = 15;
+            this.igual.Text = "=";
+            this.igual.UseVisualStyleBackColor = false;
+            this.igual.Click += new System.EventHandler(this.igual_Click);
+            // 
+            // limpar
+            // 
+            this.limpar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.limpar.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.limpar.Location = new System.Drawing.Point(44, 299);
+            this.limpar.Name = "limpar";
+            this.limpar.Size = new System.Drawing.Size(57, 23);
+            this.limpar.TabIndex = 16;
+            this.limpar.Text = "C";
+            this.limpar.UseVisualStyleBackColor = false;
+            this.limpar.Click += new System.EventHandler(this.limpar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(392, 334);
+            this.Controls.Add(this.limpar);
+            this.Controls.Add(this.igual);
+            this.Controls.Add(this.n7);
+            this.Controls.Add(this.n0);
+            this.Controls.Add(this.n9);
+            this.Controls.Add(this.n8);
+            this.Controls.Add(this.n6);
+            this.Controls.Add(this.n2);
+            this.Controls.Add(this.n5);
+            this.Controls.Add(this.n4);
+            this.Controls.Add(this.n3);
+            this.Controls.Add(this.n1);
+            this.Controls.Add(this.subtrai);
+            this.Controls.Add(this.multiplica);
+            this.Controls.Add(this.dividi);
+            this.Controls.Add(this.soma);
+            this.Controls.Add(this.tela);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label tela;
+        private System.Windows.Forms.Button soma;
+        private System.Windows.Forms.Button dividi;
+        private System.Windows.Forms.Button multiplica;
+        private System.Windows.Forms.Button subtrai;
+        private System.Windows.Forms.Button n1;
+        private System.Windows.Forms.Button n3;
+        private System.Windows.Forms.Button n4;
+        private System.Windows.Forms.Button n5;
+        private System.Windows.Forms.Button n2;
+        private System.Windows.Forms.Button n7;
+        private System.Windows.Forms.Button n0;
+        private System.Windows.Forms.Button n9;
+        private System.Windows.Forms.Button n8;
+        private System.Windows.Forms.Button n6;
+        private System.Windows.Forms.Button igual;
+        private System.Windows.Forms.Button limpar;
+    }
+}
+
---
+++ CalculadoraSimples/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraSimples
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<string> calcular= new();
+
+        #region SetNumbers
+        private void n1_Click(object sender, EventArgs e)
+        {
+            tela.Text += n1.Text;
+            
+        }
+
+        private void n2_Click(object sender, EventArgs e)
+        {
+            tela.Text += n2.Text;
+            
+        }
+
+        private void n3_Click(object sender, EventArgs e)
+        {
+            tela.Text += n3.Text;
+            
+        }
+
+        private void n4_Click(object sender, EventArgs e)
+        {
+            tela.Text += n4.Text;
+            
+        }
+
+        private void n5_Click(object sender, EventArgs e)
+        {
+            tela.Text += n5.Text;
+            
+        }
+
+        private void n6_Click(object sender, EventArgs e)
+        {
+            tela.Text += n6.Text;
+           
+        }
+
+        private void n7_Click(object sender, EventArgs e)
+        {
+            tela.Text += n7.Text;
+            
+        }
+
+        private void n8_Click(object sender, EventArgs e)
+        {
+            tela.Text += n8.Text;
+        }
+
+        private void n9_Click(object sender, EventArgs e)
+        {
+            tela.Text += n9.Text;
+           
+        }
+
+        private void n0_Click(object sender, EventArgs e)
+        {
+            tela.Text += n0.Text;
+            
+        }
+
+        private void soma_Click(object sender, EventArgs e)
+        {
+            tela.Text += soma.Text;
+            calcular.Add("+");
+        }
+
+        private void subtrai_Click(object sender, EventArgs e)
+        {
+            tela.Text += subtrai.Text;
+            calcular.Add("-");
+        }
+
+        private void multiplica_Click(object sender, EventArgs e)
+        {
+            tela.Text += multiplica.Text;
+            calcular.Add("x");
+        }
+
+        private void dividi_Click(object sender, EventArgs e)
+        {
+            tela.Text += dividi.Text;
+            calcular.Add("/");
+        }
+        #endregion
+
+        private void igual_Click(object sender, EventArgs e)
+        {
+            var result = "";
+            string[] numbers = tela.Text.Split('+','-','x','/');
+            switch (calcular[0])
+            {
+                case "+":
+                    result = (int.Parse(numbers[0]) + int.Parse(numbers[1])).ToString();
+                    limpar_Click(limpar, EventArgs.Empty);
+                    tela.Text = result;
+                    break;
+                case "-":
+                    result = (int.Parse(numbers[0]) - int.Parse(numbers[1])).ToString();
+                    limpar_Click(limpar, EventArgs.Empty);
+                    tela.Text = result;
+                    break;
+                case "x":
+                    result = (int.Parse(numbers[0]) * int.Parse(numbers[1])).ToString();
+                    limpar_Click(limpar, EventArgs.Empty);
+                    tela.Text = result;
+                    break;
+                case "/":
+                    result = (int.Parse(numbers[0]) / int.Parse(numbers[1])).ToString();
+                    limpar_Click(limpar, EventArgs.Empty);
+                    tela.Text = result;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void limpar_Click(object sender, EventArgs e)
+        {
+            tela.Text = "";
+            calcular.Clear();
+        }
+    }
+}
